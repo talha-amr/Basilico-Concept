f753197f1732ccfea01bb74a6312789c1d2a779a
Muhammad Talha Amir <tellyamir16@gmail.com>
2026-07-01 22:17:00 +0500

    fixed mobile scroll

--- src/components/CinematicScroll.jsx
+++ src/components/CinematicScroll.jsx
@@ -157,19 +157,29 @@ export default function CinematicScroll() {
         {/* Film grain overlay */}
         <div
           aria-hidden="true"
-          className="cinematic-grain"
           style={{
             position: "absolute",
             inset: 0,
             zIndex: 30,
             pointerEvents: "none",
-            backgroundImage:
-              "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='300' height='300'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.85' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='300' height='300' filter='url(%23n)'/%3E%3C/svg%3E\")",
-            backgroundRepeat: "repeat",
-            backgroundSize: "200px 200px",
-            opacity: 0.035,
           }}
-        />
+        >
+          <div
+            className="cinematic-grain"
+            style={{
+              position: "sticky",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100vh",
+              backgroundImage:
+                "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='300' height='300'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.85' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='300' height='300' filter='url(%23n)'/%3E%3C/svg%3E\")",
+              backgroundRepeat: "repeat",
+              backgroundSize: "200px 200px",
+              opacity: 0.035,
+            }}
+          />
+        </div>
 
         {/* Horizontal track on desktop, vertical on mobile */}
         <div
